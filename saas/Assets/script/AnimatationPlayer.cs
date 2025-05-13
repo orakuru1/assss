57f788cffb407e7b19c3901a2389d8dd880cde9e
Kameiyuto^ <kamei.11y17@gmail.com>
2025-05-13 09:33:47 +0900

    キャラが左クリックしたときそこに動く

--- saas/Assets/script/AnimatationPlayer.cs
+++ saas/Assets/script/AnimatationPlayer.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AnimatationPlayer : MonoBehaviour
 {
-    private Animation anim;
+    private NavMeshAgent agent;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animation>();
-
-        anim["Attack"].wrapMode = WrapMode.Once; //一回しか再生されない
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        
     }
 
-
-    public void OnAttackButtonPressed()
+    void Update()
     {
-        if(!anim.isPlaying /*連打対策*/)
+        if(Input.GetMouseButtonDown(0))
         {
-            anim.Play("Attack");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
         }
+
+        float speed = agent.velocity.magnitude;
+        if (speed < 0.1f)
+            anim.SetFloat("Run", 0f);  // Idle
+        else
+            anim.SetFloat("Run", 1f);  // Run
     }
 }
