030ea10933f61713e51f48fec72f5c9d248114f6
Kameiyuto^ <kamei.11y17@gmail.com>
2025-05-15 22:02:58 +0900

    キャラのアイコンをタップしたらキャラの詳細画面開く。開いて戻るまで

---
+++ saas/Assets/script/GetCanvas.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCanvas : MonoBehaviour
+{
+    public Canvas canvas; //変数
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.enabled = false; //スタート時非表示
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void kyaraClick()
+    {
+        canvas.enabled = !canvas.enabled; //非表示のCanvasを表示
+    }
+
+    public void onRetrun()
+    {
+        canvas.enabled = false;
+    }
+}
